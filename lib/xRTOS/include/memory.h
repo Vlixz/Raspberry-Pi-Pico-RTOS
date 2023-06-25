6677d9fa8c6c9d3dd1b596feffc45d476f00ba8d
Vlixz <boerrookhuizentim@gmail.com>
2023-06-25 13:56:48 +0200

    comments memory.h

--- lib/xRTOS/include/memory.h
+++ lib/xRTOS/include/memory.h
@@ -1,8 +1,16 @@
-#ifndef MEMORY_H
-#define MEMORY_H
+#ifndef INC_MEMORY_H
+#define INC_MEMORY_H
 
 #include <stdint.h>
 
+/**
+ * Allocates memory for a task.
+ *
+ * @param size The size of the memory to allocate.
+ * @param start_of_stack_pointer A pointer to the start of the stack pointer.
+ *
+ * @return 1 if the memory was allocated, 0 if the memory was not allocated.
+ */
 int get_task_memory(uint32_t size, uint32_t **start_of_stack_pointer);
 
-#endif // MEMORY_H
+#endif // INC_MEMORY_H
